ae47a6bccc7430127b2c5536d073b6bba37f0583
Arthur Cayet <arthurcayet@hotmail.fr>
2021-01-12 20:41:53 +0100

    affichage des recettes et de leurs description TODO css ajout et suppression recettes

--- Front/src/app/recipe/accueil/accueil.component.ts
+++ Front/src/app/recipe/accueil/accueil.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipeService } from '../recipe.service';
+import {NgbProgressbarConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-accueil',
@@ -8,22 +9,28 @@ import { RecipeService } from '../recipe.service';
 })
 export class AccueilComponent implements OnInit {
 
-  recipes
+  recipes: Array<object>
+  recipesDefined = true;
   page = 1;
   pageSize=13;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService) {}
 
   ngOnInit(): void {
     this.recipeService.getAllRecipes().subscribe(
       (data) => {
         console.log(data[1])
         this.recipes = data;
+        this.recipesDefined = false
       },
       (err) => {
         console.log(err.message);
       }
     );
   }
+
+  get getRecipes() { return (this.recipes) ? this.recipes : null }
+
+  get recipesLength() { return (this.recipes && this.recipes.length) ? this.recipes.length : null}
 
 }
